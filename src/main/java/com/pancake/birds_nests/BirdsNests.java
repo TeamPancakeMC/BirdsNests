566a81ca91e853393d96bf2f7fd527df0f83d355
XiaoHuNao <shiwenbin2250@gmail.com>
2023-09-24 01:11:05 +0800

    Migration 1.20.2

--- src/main/java/com/pancake/birds_nests/BirdsNests.java
+++ src/main/java/com/pancake/birds_nests/BirdsNests.java
@@ -1,18 +1,53 @@
 package com.pancake.birds_nests;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(BirdsNests.MOD_ID)
 public class BirdsNests {
-    public static final String MOD_ID = "birds_nests";
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public BirdsNests() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+
+	public static final String MOD_ID = "birds_nests";
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+	public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB.location(), MOD_ID);
+	public static final RegistryObject<Item> NEST = ITEMS.register("nest", ItemNest::new);
+
+	private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+	public static ForgeConfigSpec COMMON_CONFIG;
+	public static ForgeConfigSpec.DoubleValue nestRarity;
+
+	public static final RegistryObject<CreativeModeTab> TAB = TABS.register("tab",
+			() -> CreativeModeTab.builder().title(Component.translatable("itemGroup." + MOD_ID))
+					.icon(() -> NEST.get().getDefaultInstance())
+					.title(Component.translatable("itemGroup." + MOD_ID))
+					.displayItems((params, output) -> ITEMS.getEntries()
+							.forEach(item -> output.accept(item.get().getDefaultInstance()))).build())
+			;
+
+
+	public BirdsNests() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ITEMS.register(modEventBus);
+		TABS.register(modEventBus);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_CONFIG);
+	}
+
+	static {
+		COMMON_BUILDER.comment("Bird Nest Settings").push("general");
+		nestRarity = COMMON_BUILDER.comment("The rarity of nests dropping from decaying leaves [default: 0.25]").defineInRange("NEST_RARITY", 0.25, 0.0, 1.0);
+		COMMON_BUILDER.pop();
+		COMMON_CONFIG = COMMON_BUILDER.build();
+	}
 }
+
+
---
+++ src/main/java/com/pancake/birds_nests/ItemNest.java
@@ -0,0 +1,54 @@
+package com.pancake.birds_nests;
+
+
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import net.minecraftforge.server.ServerLifecycleHooks;
+public class ItemNest extends Item {
+	private static final ResourceLocation LOOT_TABLE = new ResourceLocation("birds_nests:nest_loot");
+	public ItemNest() {
+		super(new Item.Properties()
+				.stacksTo(64));
+	}
+
+	@Override
+	public InteractionResult useOn(UseOnContext context) {
+		Player player = context.getPlayer();
+		if (player != null) {
+			ItemStack stack = context.getItemInHand();
+			if (!player.isCreative()) {
+				stack.shrink(1);
+			}
+			if (!context.getLevel().isClientSide) {
+				this.generateLoot(context.getLevel(), player);
+			}
+			return InteractionResult.SUCCESS;
+		}
+        return InteractionResult.PASS;
+    }
+
+	private void generateLoot(Level level, Player player) {
+		if (LOOT_TABLE == BuiltInLootTables.EMPTY) return;
+		System.out.println("Generating loot");
+		LootTable loottable = ServerLifecycleHooks.getCurrentServer().getLootData().getLootTable(LOOT_TABLE);
+		LootParams.Builder builder = new LootParams.Builder((ServerLevel) level);
+		LootParams lootParams = builder.create(LootContextParamSet.builder().build());
+		ObjectArrayList<ItemStack> randomItems = loottable.getRandomItems(lootParams);
+		for (ItemStack itemStack : randomItems) {
+			level.addFreshEntity(new ItemEntity((ServerLevel) level, player.getX(), player.getY() + 1.5D, player.getZ(), itemStack));
+		}
+	}
+}
